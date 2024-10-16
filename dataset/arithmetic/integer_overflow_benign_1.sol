46f4dccb516cd6d63b7c67bc57f15be7148f695c
Monica Jin <monicachenjin@gmail.com>
2024-10-16 16:36:21 +0200

    port integer_overflow_benign_1.sol to solidity 0.8.0

--- dataset/arithmetic/integer_overflow_benign_1.sol
+++ dataset/arithmetic/integer_overflow_benign_1.sol
@@ -7,7 +7,7 @@
 //Single transaction overflow
 //Post-transaction effect: overflow never escapes function
 
-pragma solidity ^0.4.19;
+pragma solidity ^0.8.0;
 
 contract IntegerOverflowBenign1 {
     uint public count = 1;
